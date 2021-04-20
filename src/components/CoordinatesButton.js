8bd44ca85070e1c586c1a616f278c7c152c80d7c
Ynda Jas <yndajas@gmail.com>
2021-04-20 17:39:25 +0100

    commit tree fffda313b69f0a2810b37dcd9ae432a6fda54d55

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,15 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+    handleClick = (event) => {
+        const coordinates = [event.clientX, event.clientY];
+        this.props.onReceiveCoordinates(coordinates);
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}></button>
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+    handleClick = (event) => {
+        event.persist();
+        setTimeout(() => {
+            this.props.onDelayedClick(event);
+        }, this.props.delay);
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}></button>
+        )
+    }
+}
